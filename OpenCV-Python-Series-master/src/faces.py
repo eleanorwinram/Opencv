9632764356ad5b45b7a64b83b16a78ffc0b3137c
eleanorwinram <45363346+eleanorwinram@users.noreply.github.com>
2020-09-05 19:14:20 +0100

    faces added

--- OpenCV-Python-Series-master/src/faces.py
+++ OpenCV-Python-Series-master/src/faces.py
@@ -23,7 +23,7 @@ while(True):
     gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-    	#print(x,y,w,h)
+    	print(x,y,w,h)
     	roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
     	roi_color = frame[y:y+h, x:x+w]
 
@@ -35,13 +35,13 @@ while(True):
     		font = cv2.FONT_HERSHEY_SIMPLEX
     		name = labels[id_]
     		color = (255, 255, 255)
-    		stroke = 2
+    		stroke = 1
     		cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
 
     	img_item = "7.png"
     	cv2.imwrite(img_item, roi_color)
 
-    	color = (255, 0, 0) #BGR 0-255 
+    	color = (255, 0, 0) #BGR 0-255
     	stroke = 2
     	end_cord_x = x + w
     	end_cord_y = y + h
